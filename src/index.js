755f44638f4cc76730209cbf1a80ef9c641f8a12
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-06-21 11:30:36 +0300

    test github actions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ app.get("/", (req, res)=>
     res.json({message:"Dude Relax , Docker is easy"})
 );
 
+app.get("/test-github-actions", (req, res)=>{
+    res.json({message:"Github Actions is working"})
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=> console.log(`app listening on http://localhost:${port}`))
